62195ca7481a8202335f9d3004cd7491c2c1b491
Zac Petro <zmpetro@gmail.com>
2023-09-24 19:09:04 -0400

    impl main.rs

--- 07/vm_translator_rs/src/main.rs
+++ 07/vm_translator_rs/src/main.rs
@@ -1,5 +1,19 @@
 mod vm_translator;
 
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        panic!("Usage: vm_translator_rs <infile> <outfile>");
+    }
+    let infile = &args[1];
+    let outfile = &args[2];
+    let asm_output = vm_translator::translate(infile);
+    println!(
+        "Translating {} and writing hack assembly output to {}...",
+        infile, outfile
+    );
+    vm_translator::write_lines(outfile, &asm_output);
+    println!("Translation successful; output written to {}", outfile);
 }
