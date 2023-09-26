e9145faab0525a388f6836fc0f07a4201b2ba39b
Zac Petro <zmpetro@gmail.com>
2023-09-25 21:39:53 -0400

    vm_translator_rs: update main to conform with specifications

--- 07/vm_translator_rs/src/main.rs
+++ 07/vm_translator_rs/src/main.rs
@@ -4,16 +4,17 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        panic!("Usage: vm_translator_rs <infile> <outfile>");
+    if args.len() != 2 {
+        panic!("Usage: vm_translator_rs <infile>");
     }
     let infile = &args[1];
-    let outfile = &args[2];
-    let asm_output = vm_translator::translate(infile);
+    let outfile: Vec<&str> = infile.split(".").collect();
+    let outfile = format!("{}.asm", outfile[0]);
     println!(
         "Translating {} and writing hack assembly output to {}...",
         infile, outfile
     );
-    vm_translator::write_lines(outfile, &asm_output);
+    let asm_output = vm_translator::translate(infile);
+    vm_translator::write_lines(&outfile, &asm_output);
     println!("Translation successful; output written to {}", outfile);
 }
