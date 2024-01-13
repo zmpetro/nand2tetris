d8360990e321fb9566f8052668afd85429a60018
Zac Petro <zmpetro@gmail.com>
2024-01-13 11:18:12 -0500

    analyze file or directory

--- 10/syntax_analyzer_rs/src/main.rs
+++ 10/syntax_analyzer_rs/src/main.rs
@@ -26,7 +26,7 @@ fn write_lines(outfile: &PathBuf, xml_output: &[String]) {
     for line in xml_output {
         let event = make_event_from_line(line);
         if let Err(e) = writer.write(event) {
-            panic!("Failed to write jack xml output: {e}");
+            panic!("Failed to write xml output: {e}");
         }
     }
 }
@@ -34,19 +34,34 @@ fn write_lines(outfile: &PathBuf, xml_output: &[String]) {
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        panic!("Usage: syntax_analyzer_rs <infile>");
+        panic!("Usage: syntax_analyzer_rs <infile or directory>");
     }
-    let infile = Path::new(&args[1]);
-    let outfile = infile.with_extension("xml");
-    println!(
-        "Analzying {} and writing jack xml output to {} ...",
-        infile.to_str().unwrap(),
-        outfile.to_str().unwrap()
-    );
-    let xml_output = syntax_analyzer::analyze_file(infile);
-    write_lines(&outfile, &xml_output);
-    println!(
-        "Analysis successful; output written to {}",
-        outfile.to_str().unwrap()
-    );
+    let infile_or_directory = Path::new(&args[1]);
+    let mut files_to_analyze: Vec<PathBuf> = vec![];
+    if infile_or_directory.is_dir() {
+        for entry in infile_or_directory.read_dir().unwrap() {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+                if path.extension().unwrap() == "jack" {
+                    files_to_analyze.push(path);
+                }
+            }
+        }
+    } else {
+        files_to_analyze.push(infile_or_directory.to_path_buf());
+    };
+    for infile in files_to_analyze {
+        let outfile = infile.with_extension("xml");
+        println!(
+            "Analzying {} and writing xml output to {} ...",
+            infile.to_str().unwrap(),
+            outfile.to_str().unwrap()
+        );
+        let xml_output = syntax_analyzer::analyze_file(&infile);
+        write_lines(&outfile, &xml_output);
+        println!(
+            "Analysis successful; output written to {}",
+            outfile.to_str().unwrap()
+        );
+    }
 }
